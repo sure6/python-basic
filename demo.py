e15d2fcf4b01275118c5ff74b5da1d35c80d4f71
Xuan Li <31686128+sure6@users.noreply.github.com>
2022-12-18 00:41:50 +1100

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,16 +1,31 @@
-"""
-date: 2021/4/27 14:06
-author: lee sure
-"""
+# The 1st column must use right alignment.
+# The 2nd column must use center alignment.
+# The 3rd column must use left alignment.
+# The 4th column must use right alignment.
 
-with open("C:\\python-workspace\\pyFileIO\\demo.txt", 'r') as f:
-    # f.seek(0,2)
-    # print(f.tell())
-    # f.write("fgh".encode())
-    # f.write("asa".encode())
-    for i in f:
-        print(i.rstrip())
+# file name: book.csv
+# Enter 1st column length: 15
+# Enter 2nd column length: 25
+# Enter 3rd column length: 20
+# Enter 4th column length: 10
 
-    # f.write("hello, python")
-    # f.seek()
 
+import csv
+
+f_name=input("Enter file name: ")
+f_col=input("Enter 1st column length: ")
+s_col=input("Enter 2nd column length: ")
+t_col=input("Enter 3rd column length: ")
+four_col=input("Enter 4th column length: ")
+
+print(("{0:>"+f_col+"}{1:^"+s_col+"}{2:<"+t_col+"}{3:>"+four_col+"}").format("Location","Book title","Publication Date", "Available"))
+
+with open(f_name,'r') as f:
+    reader = csv.DictReader(f)
+
+    for row in reader:
+        shelf= row.get('SHELF')
+        title= row.get('TITLE')
+        date= row.get('DATE')
+        avail= row.get('AVAILABLE')
+        print(("{0:>"+f_col+"}{1:^"+s_col+"}{2:<"+t_col+"}{3:>"+four_col+"}").format(shelf,title,date,avail))
